1d555f2b8f6d698ed0c8810efdaa640884ed02f3
Naomi Dennis <njdennis94@gmail.com>
2018-04-17 21:40:45 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,5 +6,5 @@ class Song < ActiveRecord::Base
   validates :genre, presence: true
   validates :released, inclusion: {in: [true, false] }
   validates_presence_of :release_year, if: Proc.new{|obj| obj[:released] == true}
-
+  validates_inclusion_of :release_year, in: => 0.99
 end
